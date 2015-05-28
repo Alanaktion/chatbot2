e170ec1e35cd92be229232b8988d790988c23b17
Alan Hardman <ahardman@thrivelife.com>
2015-05-28 12:08:05 -0600

    Initial stuffs

---
+++ bot.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Chatbot2
+ * @author Alan Hardman <alan@phpizza.com>
+ */
+
+require __DIR__ . '/vendor/autoload.php';
---
+++ readme.md
@@ -0,0 +1,4 @@
+# chatbot2
+
+## Installation
+Run a git clone, then `compser install`.
