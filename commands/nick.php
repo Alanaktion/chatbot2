e2b1a34d57622af431a0013f2f2317c1081c6ec8
Alan Hardman <ahardman@thrivelife.com>
2015-05-29 10:37:46 -0600

    Fixing #nick

--- commands/nick.php
+++ commands/nick.php
@@ -1,11 +1,11 @@
 <?php
 return function(JAXL $client, XMPPStanza $msg, array $params) {
+	global $config;
 	if(empty($params[0])) {
 		return "Usage: #nick <nickname>";
 	}
 
-	$config = Bot::config();
-	$client->xep['0045']->join_room(
+	$client->xeps['0045']->join_room(
 		$config["muc"]["room"] . '@' . $config["muc"]["server"] . '/' . $params[0],
 		array('no_history' => true) + $config["muc"]["password"] ? array('password' => $config["muc"]["password"]) : array()
 	);
