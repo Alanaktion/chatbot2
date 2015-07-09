cb5e2bdefe2e2c7df69877340bdbce2de3202b3b
Alan Hardman <ahardman@thrivelife.com>
2015-07-09 13:52:45 -0600

    Fixing #ping for timed out requests

--- commands/ping.php
+++ commands/ping.php
@@ -2,7 +2,7 @@
 return function(JAXL $client, XMPPStanza $msg, array $params) {
 	if (!empty($params[0])) {
 		$response = json_decode(BotHttp::GET("http://phpizza.com/~alan/ping.php?host=" . urlencode($params[0])));
-		if($response->online) {
+		if($response && $response->online) {
 			return "Online - " . $response->ping;
 		} else {
 			return "Unable to connect to host.";
