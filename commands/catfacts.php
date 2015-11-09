8a8871089695ac16b0917a6f045b02d402b98eca
Alan Hardman <alanaktion@gmail.com>
2015-11-08 20:56:35 -0700

    Fixing [num] param on #catfacts

--- commands/catfacts.php
+++ commands/catfacts.php
@@ -1,6 +1,6 @@
 <?php
 return function(JAXL $client, XMPPStanza $msg, array $params) {
-	$number = (isset($params[0]) && $params[0] > 0) ? max(100, $params[0]) : 0;
+	$number = (isset($params[0]) && $params[0] > 0) ? min(100, $params[0]) : 0;
 	$response = BotHttp::GET("https://catfacts-api.appspot.com/api/facts" . ($number ? '?number=' . $number : ''));
 	$obj = json_decode($response);
 	return implode("\n", $obj->facts);
