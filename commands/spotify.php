48c5f1264e535f36c2b3c287ee8c5fbce96140a2
Alan Hardman <ahardman@thrivelife.com>
2016-02-15 09:59:20 -0700

    Updating #spotify

--- commands/spotify.php
+++ commands/spotify.php
@@ -1,8 +1,16 @@
 <?php
 return function(JAXL $client, XMPPStanza $msg, array $params) {
+	$q = implode(" ", $params);
+	$type = "album,artist,track";
+	if(preg_match("/(track|song|artist|album|playlist)/", $q, $matches)) {
+		$type = $matches[1];
+		if($type == "song")
+			$type = "track";
+		$q = ltrim(str_replace($matches[0], "", $q));
+	}
 	$options = array(
-		"q" => implode(" ", $params),
-		"type" => "track",
+		"q" => $q,
+		"type" => $type,
 		"market" => "US",
 		"limit" => 1,
 		"offset" => 0
@@ -12,6 +20,26 @@ return function(JAXL $client, XMPPStanza $msg, array $params) {
 	);
 	$req = \BotHTTP::GET("https://api.spotify.com/v1/search?" . http_build_query($options), $headers);
 	$obj = json_decode($req);
-	$track = $obj->tracks->items[0];
-	return $track->artists[0]->name . " - " . $track->name . ' - ' . $track->external_urls->spotify;
+	switch($type) {
+		case "track":
+			$item = $obj->tracks->items[0];
+			return $item->artists[0]->name . " - " . $item->name . " - " . $item->external_urls->spotify;
+		case "artist":
+			$item = $obj->artists->items[0];
+			return $item->name . " - " . $item->external_urls->spotify;
+		case "album":
+			$item = $obj->albums->items[0];
+			return $item->name . " - " . $item->external_urls->spotify;
+		case "playlist":
+			$item = $obj->playlists->items[0];
+			return $item->name . " - " . $item->external_urls->spotify;
+		default:
+			$item = $obj->tracks->items[0];
+			$return  = "\nTrack: " . $item->artists[0]->name . " - " . $item->name . " - " . $item->external_urls->spotify;
+			$item = $obj->artists->items[0];
+			$return .= "\nArtist: " . $item->name . " - " . $item->external_urls->spotify;
+			$item = $obj->albums->items[0];
+			$return .= "\nAlbum: " . $item->name . " - " . $item->external_urls->spotify;
+		return $return;
+	}
 };
